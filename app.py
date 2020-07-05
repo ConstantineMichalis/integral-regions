a32e6fee3c6cb9554b9a397f977e7286da3d9f3d
ConstantineMichalis <66834305+ConstantineMichalis@users.noreply.github.com>
2020-07-05 11:31:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 12 16:32:05 2020
-
-@author: cmichalis
-"""
-
-import flask; 
-import plotly.express as px
-import dash; 
-import dash_core_components as dcc; 
-import dash_html_components as html 
+import flask; import plotly.express as px
+import dash; import dash_core_components as dcc; import dash_html_components as html
 from dash.dependencies import Input, Output
 
-server =flask.Flask(__name__)
+# Setup the app. The server & app names should match those in Procfile 
+server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server)
-tips= px.data.tips();  
+
+tips= px.data.tips()
 fig = px.scatter(tips, x="total_bill", y="tip")
-app.layout= html.Div([ dcc.RadioItems(id="gender1", 
-                                      options=[{'label': 'Female', 'value': 'Female'},
-                                               {'label': 'Male', 'value': 'Male'}], 
-                                                value='Female'),
-                                               dcc.Graph(id="fig1", figure=fig)])
-                                                
+
+app.layout = html.Div([
+  dcc.RadioItems(id="gender1", options=[{'label': 'Female', 'value': 'Female'},{'label': 'Male', 'value': 'Male'}], value='Female'),
+  dcc.Graph(id="fig1", figure=fig)])
+
 @app.callback(Output('fig1', 'figure'),[Input('gender1', 'value')])
-def updateGender(g):    return  px.scatter(tips.query("sex=='"+g+"'"), x="total_bill", y="tip")
+def updateGender(g):
+    return  px.scatter(tips.query("sex=='"+g+"'"), x="total_bill", y="tip")
+
+
+# Run the Dash app
 if __name__ == '__main__':
-    app.server.run(debug=True, use_reloader=False) # Run the Dash app
-    
+    app.server.run(debug=True, use_reloader=False)
